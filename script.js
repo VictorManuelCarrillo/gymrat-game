54d445d14f5c7b3e5e868430239eb2c79ecd2cbc
victor carrillo <gatitto079@gmail.com>
2024-07-29 16:41:03 -0600

    added video and song

--- script.js
+++ script.js
@@ -11,8 +11,17 @@ const exercises = [
   { description: "Do a 30-second mountain climber", image: "https://via.placeholder.com/150?text=Mountain+climber" }
 ];
 
-let exerciseCount = 0;
-let round = 1;
+let currentPlayer = 1;
+let exerciseCount = [0, 0];
+let players = ["Player 1", "Player 2"];
+
+document.getElementById('startGame').addEventListener('click', () => {
+  players[0] = document.getElementById('player1').value || "Player 1";
+  players[1] = document.getElementById('player2').value || "Player 2";
+  document.getElementById('turnIndicator').innerText = `${players[0]}'s Turn`;
+  document.getElementById('startGame').classList.add('hidden');
+  document.getElementById('generateExercise').classList.remove('hidden');
+});
 
 document.getElementById('generateExercise').addEventListener('click', () => {
   const randomIndex = Math.floor(Math.random() * exercises.length);
@@ -20,24 +29,43 @@ document.getElementById('generateExercise').addEventListener('click', () => {
   document.getElementById('exerciseDisplay').innerText = exercise.description;
   document.getElementById('exerciseImage').innerHTML = `<img src="${exercise.image}" alt="${exercise.description}" class="mx-auto mt-4">`;
   document.getElementById('actionButtons').classList.remove('hidden');
+  document.getElementById('generateExercise').classList.add('hidden');
 });
 
 document.getElementById('continueBtn').addEventListener('click', () => {
-  exerciseCount++;
-  round++;
-  document.getElementById('result').innerText = `Exercises performed: ${exerciseCount}`;
+  exerciseCount[currentPlayer - 1]++;
+  updateStats();
+  switchPlayer();
   document.getElementById('exerciseDisplay').innerText = '';
   document.getElementById('exerciseImage').innerHTML = '';
-  document.getElementById('roundCounter').innerText = `Round: ${round}`;
   document.getElementById('actionButtons').classList.add('hidden');
+  document.getElementById('generateExercise').classList.remove('hidden');
 });
 
 document.getElementById('giveUpBtn').addEventListener('click', () => {
-  document.getElementById('result').innerText = `You performed ${exerciseCount} exercises. Better luck next time!`;
+  document.getElementById('result').innerText = `You have lost, ${players[currentPlayer - 1]} with a 35cm arm!`;
+  resetGame();
+});
+
+function switchPlayer() {
+  currentPlayer = currentPlayer === 1 ? 2 : 1;
+  document.getElementById('turnIndicator').innerText = `${players[currentPlayer - 1]}'s Turn`;
+}
+
+function updateStats() {
+  document.getElementById('player1Stats').innerText = `${players[0]} Exercises: ${exerciseCount[0]}`;
+  document.getElementById('player2Stats').innerText = `${players[1]} Exercises: ${exerciseCount[1]}`;
+}
+
+function resetGame() {
+  currentPlayer = 1;
+  exerciseCount = [0, 0];
+  players = ["Player 1", "Player 2"];
+  document.getElementById('turnIndicator').innerText = `Player 1's Turn`;
   document.getElementById('exerciseDisplay').innerText = '';
   document.getElementById('exerciseImage').innerHTML = '';
   document.getElementById('actionButtons').classList.add('hidden');
-  exerciseCount = 0;
-  round = 1;
-  document.getElementById('roundCounter').innerText = `Round: ${round}`;
-});
+  document.getElementById('generateExercise').classList.add('hidden');
+  document.getElementById('startGame').classList.remove('hidden');
+  updateStats();
+}
